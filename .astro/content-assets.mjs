031f51531573fd8194b266566501faf777aa39f3
Stefan Carpentier <stefancarpentier@Stefan-miniMAC-6.local>
2026-08-31 22:41:25 +0200

    vault backup: 2026-08-31 22:41:25

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_0 from "@/assets/images/astropaper-og.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fabout.md";
-export default new Map([["@/assets/images/astropaper-og.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fabout.md", __ASTRO_IMAGE_IMPORT_0]]);
+import __ASTRO_IMAGE_IMPORT_0 from "./husky.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FCAPS+2026.md";
+import __ASTRO_IMAGE_IMPORT_1 from "@/assets/images/astropaper-og.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fabout.md";
+export default new Map([["./husky.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FCAPS+2026.md", __ASTRO_IMAGE_IMPORT_0], ["@/assets/images/astropaper-og.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fabout.md", __ASTRO_IMAGE_IMPORT_1]]);
 		
